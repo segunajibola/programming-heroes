382a1b3bad5d5605c9928becced0d7da6df1dcdc
Segun Ajibola <ajbl.segun@gmail.com>
2021-10-01 01:43:33 +0100

    first commit

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-i// import { useState } from "react";
+// import { useState } from "react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
